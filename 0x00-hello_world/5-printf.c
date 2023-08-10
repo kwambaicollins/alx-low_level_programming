6ddcc9a52766f30e755a7ed3e9465deb1582c9b8
kwambaicollins <kwambaicollins3@gmail.com>
2023-08-10 14:50:58 -0700

    my commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,11 +6,11 @@
 int main(void)
 {
 
-char a;
-int b;
-long int c;
-long long int d;
-float f;
+   char a;
+   int b;
+   long int c;
+   long long int d;
+   float f;
 
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of an int: %lu byte(s)\n", ()unsigned long)sizeof(b));
