97aa4fb2ef128cf4df6b7171103bbe186b3a6c3b
kwambaicollins <kwambaicollins3@gmail.com>
2023-08-10 13:52:53 -0700

    My first c commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.n>
+/**
+*main - A betty compilant program that prints a line with puts functions
+*Return: 0(correct)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
