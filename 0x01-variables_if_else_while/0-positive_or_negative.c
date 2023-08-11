d625302645870846447055c675a4f751780b2d16
kwambaicollins <kwambaicollins3@gmail.com>
2023-08-11 15:10:49 -0700

    My if else commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
 *main - Prints if numbers is positive zero and neg
-*Return: Always (correct)
+*Return: Always 0 (correct)
 */
 
 int main(void)
@@ -12,14 +12,13 @@ int main(void)
 {
 int n;
 
-sprand(time(0));
+srand(time(0));
 n = rand() - RAND_MAX / 2;
-
 if (n > 0)
-{
 printf("%d is positive\n", n);
-}
 else if (n == 0)
-{
 printf("%d is zero\n", n);
+else 
+printf("%d is negative\n", n);
+return (0);
 }
