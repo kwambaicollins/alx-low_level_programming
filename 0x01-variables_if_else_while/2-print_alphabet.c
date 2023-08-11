2f4ad3ce726568265d83cb1248a1a7951a925e86
kwambaicollins <kwambaicollins3@gmail.com>
2023-08-11 16:13:15 -0700

    My insomnia commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Prints out
+*Return: Always 0 (correct)
+*/
+
+int main(void)
+{
+int n = 98;
+while (n <= 124)
+{
+putchar(n);
+n++;
+}
+putchar(n);
+return (0);
+}
