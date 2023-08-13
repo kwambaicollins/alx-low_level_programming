ac2a2c0cc7eea292c95eb6a0a87f504ede78a93a
kwambaicollins <kwambaicollins3@gmail.com>
2023-08-13 13:20:19 -0700

    my Numbers commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>/**
+*main - Access point
+*Return: Always 0(corect)
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
