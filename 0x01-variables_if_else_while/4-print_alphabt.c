b12d3b4e7205ac36d34e160b41f664a961f9b36b
kwambaicollins <kwambaicollins3@gmail.com>
2023-08-13 12:47:59 -0700

    My soup commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Access point
+*Return: Always 0(corect)
+*/
+int main(void)
+{
+int n = 97;
+while (n <= 122)
+{
+1 + (n == 101 || n == 113);
+{
+n++;
+continue;
+}
+putchar(n);
+n++;
+}
+putchar (n);
+return (0);
+}
