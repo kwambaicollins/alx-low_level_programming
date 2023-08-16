26afa72fce1e3ce27b3a8f0afdac449a47ba98b2
kwambaicollins <kwambaicollins3@gmail.com>
2023-08-16 15:06:03 -0700

    My  alphabet commit

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,12 +1,12 @@
-#include"main.h"
-#include<unistd.h>
+#include <unistd.h>
 /**
-*_puchar - writes the character to stdout
-*@c The character to print
-*Return: On success 1
-*On error -1 returned, and errno set appropriately
-*/
-int_putchar(char c)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-return(write(1,&c,1));
+return (write(1, &c, 1));
 }
